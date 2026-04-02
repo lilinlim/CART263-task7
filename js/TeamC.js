c1c8650161af8bb6fd04fba5748ff9d8bddddda1
allecatt <157419169+kaikukuu@users.noreply.github.com>
2026-04-02 15:48:29 -0400

    Started step 2 of creating the moons and roatating them around the planet

--- js/TeamC.js
+++ js/TeamC.js
@@ -10,7 +10,7 @@ export class PlanetC {
 
         //Create planet group
         this.group = new THREE.Group()
-              
+
         // Create planet
         //STEP 1:
         //TODO: Create a planet using THREE.SphereGeometry (Radius must be between 1.5 and 2).
@@ -21,28 +21,62 @@ export class PlanetC {
         //STEP 2: 
         //TODO: Add from 1 to 3 orbiting moons to the planet group. 
         //TODO: The moons should rotate around the planet just like the planet group rotates around the Sun.
+        //make 3 moons and make them orbit around planet c
+        //creates 3 moons with random sizes, colors, and orbit speeds
+        for (let i = 0; i < 3; i++) {
+            //creates a moon with random size and color
+            const moonGeometry = new THREE.SphereGeometry(0.3 + Math.random() * 0.2, 16, 16);
+            //creates a material with random color
+            const moonMaterial = new THREE.MeshStandardMaterial({ color: Math.random() * 0xffffff });
+            //creates the moon mesh and sets it to cast and receive shadows
+            const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+            //sets the moon to cast and receive shadows
+            moon.castShadow = true;
+            //sets the moon to receive shadows
+            moon.receiveShadow = true;
+            //sets random orbit and radius for the moon
+            const moonOrbitRadius = 2 + Math.random() * 1;
+            //sets random orbit speed for the moon
+            const moonOrbitSpeed = 0.5 + Math.random() * 0.5;
+            //stores the moon's orbit radius, speed, and initial angle in userData for use in the update method
+            moon.userData = { orbitRadius: moonOrbitRadius, orbitSpeed: moonOrbitSpeed, angle: Math.random() * Math.PI * 2 };
+            this.group.add(moon);
+        } // we make the moons orbit around the planet in the update method 
+
+
+
+
 
         //STEP 3:
         //TODO: Load Blender models to populate the planet with multiple props and critters by adding them to the planet group.
         //TODO: Make sure to rotate the models so they are oriented correctly relative to the surface of the planet.
-        
+
         //STEP 4:
         //TODO: Use raycasting in the click() method below to detect clicks on the models, and make an animation happen when a model is clicked.
         //TODO: Use your imagination and creativity!
 
         this.scene.add(this.group);
     }
-    
+
     update(delta) {
         // Orbit around sun
         this.angle += this.orbitSpeed * delta * 30;
         this.group.position.x = Math.cos(this.angle) * this.orbitRadius;
         this.group.position.z = Math.sin(this.angle) * this.orbitRadius;
-        
+
         // Rotate planet
-        this.group.rotation.y += delta*0.5;
+        this.group.rotation.y += delta * 0.5;
 
         //TODO: Do the moon orbits and the model animations here.
+        //makes all the moons orbit around the planet
+        this.group.children.forEach(child => {
+            if (child.userData.orbitRadius) {
+                child.userData.angle += child.userData.orbitSpeed * delta * 3;
+                child.position.x = Math.cos(child.userData.angle) * child.userData.orbitRadius;
+                child.position.z = Math.sin(child.userData.angle) * child.userData.orbitRadius;
+            }
+        });
+
     }
 
     click(mouse, scene, camera) {
